414059b8b46b921d696e7cae3213859af459b79e
lazy-sky <1cielo@naver.com>
2022-05-05 01:36:15 +0900

    Feat: Render the message if there's no tasks

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -16,18 +16,22 @@ function Tasks({ tasks, setTasks }) {
   return (
     <div className={styles.container}>
       <SectionTitle title="TODAY'S TASKS" />
-      <ul className={styles.taskList}>
-        {tasks.map(({ id, isDone, category, content }) => (
-          <TaskItem
-            key={`task-${id}`}
-            isDone={isDone}
-            category={category}
-            content={content}
-            onToggle={() => handleToggleTask(id)}
-            onClick={() => handleClickDeleteButton(id)}
-          />
-        ))}
-      </ul>
+      {tasks.length ? (
+        <ul className={styles.taskList}>
+          {tasks.map(({ id, isDone, category, content }) => (
+            <TaskItem
+              key={`task-${id}`}
+              isDone={isDone}
+              category={category}
+              content={content}
+              onToggle={() => handleToggleTask(id)}
+              onClick={() => handleClickDeleteButton(id)}
+            />
+          ))}
+        </ul>
+      ) : (
+        <div style={{ marginTop: '20px' }}>할 일을 다하다니 부지런하군!</div>
+      )}
     </div>
   )
 }
