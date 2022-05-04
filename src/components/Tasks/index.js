1652cce6f124a7d45306e0ae0aaea4bbd440eb07
lazy-sky <1cielo@naver.com>
2022-05-04 16:14:11 +0900

    Feat: 홈 화면 레이아웃 구조

---
+++ src/components/Tasks/index.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react/prop-types
+function Tasks({ tasks }) {
+  return <div />
+}
+
+export default Tasks
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,34 @@
+import { useState } from 'react'
 import styles from './Routes.module.scss'
 
+import Header from '../components/Header'
+import WelcomeMessage from '../components/WelcomeMessage'
+import Categories from '../components/Categories'
+import Tasks from '../components/Tasks'
+
+// TODO: Make CreateTaskButton
+// TODO: Make SectionTitle
+
 function App() {
-  return <div className={styles.main}>메인</div>
+  const [userName, setUserName] = useState('Joy')
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      date: '',
+      isDone: false,
+      category: 'personal',
+      content: 'This is the first task',
+    },
+  ])
+
+  return (
+    <div className={styles.main}>
+      <Header />
+      <WelcomeMessage userName={userName} />
+      <Categories />
+      <Tasks tasks={tasks} />
+    </div>
+  )
 }
 
 export default App
