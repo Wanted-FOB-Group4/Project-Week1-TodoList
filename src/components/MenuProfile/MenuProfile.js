f57bc04f2ec3d4da5106751a1eeef8c008f81dbe
chichoon <chichoon.choi@gmail.com>
2022-05-05 17:42:33 +0900

    Feat: 총 투두 개수, 완료한 투두 개수 추가, useEffect percent 계산

--- src/components/MenuProfile/MenuProfile.js
+++ src/components/MenuProfile/MenuProfile.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import styles from './MenuProfile.module.scss'
 
 import { CircularProgressbar } from 'react-circular-progressbar'
@@ -5,12 +6,18 @@ import CIRCLE_STYLE from './progressBarStyle'
 import ArrowSvg from './ArrowSvg'
 
 // eslint-disable-next-line react/prop-types
-function MenuProfile({ name, picSrc }) {
+function MenuProfile({ name, picSrc, todoMax, todoCount }) {
+  const [percentage, setPercentage] = useState(0)
+
+  useEffect(() => {
+    setPercentage((todoCount * 100) / todoMax)
+  }, [todoMax, todoCount])
+
   return (
     <div className={styles.profileDiv}>
       <div className={styles.profileSect}>
         <div className={styles.imageDiv}>
-          <CircularProgressbar value={30} className={styles.progressBar} styles={CIRCLE_STYLE} />
+          <CircularProgressbar value={percentage} className={styles.progressBar} styles={CIRCLE_STYLE} />
           <img src={picSrc} alt='profilePic' />
         </div>
         <h1>{name}</h1>
