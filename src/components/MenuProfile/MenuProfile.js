ca5a4091c7914d9367a57a5fedc6e8d29e11b83c
chichoon <chichoon.choi@gmail.com>
2022-05-05 11:52:31 +0900

    Feat: prop types 검사 disable

--- src/components/MenuProfile/MenuProfile.js
+++ src/components/MenuProfile/MenuProfile.js
@@ -1,10 +1,39 @@
 import styles from './MenuProfile.module.scss'
 
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar'
+
+const CIRCLE_STYLE = {
+  root: {},
+  path: {
+    stroke: 'rgba(62, 152, 199, 100%)',
+    strokeLinecap: 'butt',
+    transition: 'stroke-dashoffset 0.5s ease 0s',
+    transform: 'rotate(0.25turn)',
+    transformOrigin: 'center center',
+  },
+  trail: {
+    stroke: '#d6d6d6',
+    strokeLinecap: 'butt',
+    transform: 'rotate(0.25turn)',
+    transformOrigin: 'center center',
+  },
+  text: {
+    fill: '#f88',
+    fontSize: '16px',
+  },
+  background: {
+    fill: '#3e98c7',
+  },
+}
+
+// eslint-disable-next-line react/prop-types
 function MenuProfile({ name, picSrc }) {
   return (
     <div className={styles.profileDiv}>
       <div className={styles.profileSect}>
-        <img src={picSrc} alt='profilePic' />
+        <CircularProgressbarWithChildren value={66} className={styles.progressBar} styles={CIRCLE_STYLE}>
+          <img src={picSrc} alt='profilePic' />
+        </CircularProgressbarWithChildren>
         <h1>{name}</h1>
       </div>
       <button type='button' className={styles.buttonWrapper}>
@@ -12,11 +41,6 @@ function MenuProfile({ name, picSrc }) {
       </button>
     </div>
   )
-}
-
-MenuProfile.propTypes = {
-  name: String,
-  picSrc: String,
 }
 
 export default MenuProfile
