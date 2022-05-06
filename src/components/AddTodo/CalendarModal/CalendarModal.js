f68b9b23f178e4d72a1be597b0d8356fd6add531
Han Byul <yahb9@naver.com>
2022-05-06 16:47:04 +0900

    Fix: 날짜 데이터 포맷 함수 및 적용

--- src/components/AddTodo/CalendarModal/CalendarModal.js
+++ src/components/AddTodo/CalendarModal/CalendarModal.js
@@ -3,25 +3,39 @@ import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css' 
 import styles from './CalendarModal.module.scss'
 
-function CalendarModal({setSelectDate}) {
+function CalendarModal(setSelectDate) {
   const [startDate, setStartDate] = useState(new Date())
-  // const [selectDate, setSelectDate] = useState(new Date())
+//   const [selectDate, setSelectDate] = useState(dateFormat(new Date()))
 
-  // const setSelectDate = (day) => {
-  //   setSelectDate(day)
-  // }
+//   const dateFormat = (date) => {
+//     let year = date.getFullYear()
+//     let month = date.getMonth() + 1
+//     let day = date.getDate()
+//     let hour = date.getHours()
+//     let minute = date.getMinutes()
+//     let second = date.getSeconds()
+
+//     year = year.toString().substr(2,4)
+//     month = month >= 10 ? month : `0${month}`
+//     day = day >= 10 ? day : `0${day}`
+//     hour = hour >= 10 ? hour : `0${hour}`
+//     minute = minute >= 10 ? minute : `0${minute}`
+//     second = second >= 10 ? second : `0${second}`
+
+//     return (year + month + day + hour + minute + second).toString()
+// }
+
+   const setSelectDates = (date) => {
+    //  setSelectDate(dateFormat(date))
+   }
 
   const handleDateChange = (date) => {
     setStartDate(date)
   }
 
   return ( 
-    <span><DatePicker className={styles.date} dateFormat='yyyy/MM/dd' selected={startDate} onSelect={setSelectDate} onChange={date => handleDateChange(date)} /></span> 
+    <span><DatePicker className={styles.date} dateFormat='yyyy/MM/dd' selected={startDate} onSelect={setSelectDates} onChange={date => handleDateChange(date)} /></span> 
   )
-}
-
-CalendarModal.propTypes = {
-  setSelectDate: Function
 }
 
 export default CalendarModal
