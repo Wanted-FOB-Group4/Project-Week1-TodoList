2dc4459377ce480d5e9a843b057780dad0f25dba
Eunchae Ko <kec0130@gmail.com>
2022-05-06 12:36:32 +0900

    Feat: 스와이프 삭제 버튼 구현

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,1 +1,2 @@
 export { ReactComponent as LogoImage } from './logo.svg'
+export { ReactComponent as TrashImage } from './trash-can-solid.svg'
--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,36 +1,40 @@
 import SectionTitle from '../SectionTitle'
 import styles from './Tasks.module.scss'
-import Swipe from '../Swipe'
 import TaskItem from '../TaskItem'
+import { SwipeableList, SwipeableListItem, SwipeAction, TrailingActions, Type as ListType } from 'react-swipeable-list'
+import 'react-swipeable-list/dist/styles.css'
+import { TrashImage } from '../../assets/svgs'
 
 function Tasks({ tasks, setTasks }) {
   const handleToggleTask = (id) => {
     setTasks((prevTasks) => prevTasks.map((task) => (task.id === id ? { ...task, isDone: !task.isDone } : task)))
   }
 
-  const handleSwipeLeftTask = (id) => {
+  const handleDelete = (id) => {
     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id))
   }
+
+  const trailingActions = ({ id }) => (
+    <TrailingActions>
+      <SwipeAction destructive onClick={() => handleDelete(id)}>
+        <div className={styles.deleteButton}>
+          <TrashImage className={styles.trashIcon} aria-label='delete' />
+        </div>
+      </SwipeAction>
+    </TrailingActions>
+  )
 
   return (
     <div className={styles.container}>
       <SectionTitle title="TODAY'S TASKS" />
       {tasks.length ? (
-        <ul className={styles.taskList}>
+        <SwipeableList className={styles.taskList} fullSwipe threshold={0.5} type={ListType.IOS}>
           {tasks.map(({ id, isDone, category, content }) => (
-            // TODO: 스와이프 후 딜레이, undo 할 수 있도록
-            // TODO: 스와이프 디자인
-            <Swipe onSwipeLeft={() => handleSwipeLeftTask(id)}>
-              <TaskItem
-                key={`task-${id}`}
-                isDone={isDone}
-                category={category}
-                content={content}
-                onToggle={() => handleToggleTask(id)}
-              />
-            </Swipe>
+            <SwipeableListItem key={`task-${id}`} trailingActions={trailingActions({ id })}>
+              <TaskItem isDone={isDone} category={category} content={content} onToggle={() => handleToggleTask(id)} />
+            </SwipeableListItem>
           ))}
-        </ul>
+        </SwipeableList>
       ) : (
         <div style={{ marginTop: '20px' }}>할 일을 다하다니 부지런하군!</div>
       )}
