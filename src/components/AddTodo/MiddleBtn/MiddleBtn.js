5405c1d4ce61bb623ad3cd74cd5b41fd0f6248f9
Han Byul <yahb9@naver.com>
2022-05-08 03:11:24 +0900

    Fix: className 수정

--- src/components/AddTodo/MiddleBtn/MiddleBtn.js
+++ src/components/AddTodo/MiddleBtn/MiddleBtn.js
@@ -3,7 +3,7 @@ import styles from './MiddleBtn.module.scss'
 // eslint-disable-next-line react/prop-types
 function MiddleBtn({ children, shape, onClose }) {
   return (
-    <button className={shape === 'circle' ? styles.circlebtn : styles.ovalbtn} type='button' onClick={onClose}>
+    <button className={shape === 'circle' ? styles.circleBtn : styles.ovalBtn} type='button' onClick={onClose}>
       {children}
     </button>
   )
