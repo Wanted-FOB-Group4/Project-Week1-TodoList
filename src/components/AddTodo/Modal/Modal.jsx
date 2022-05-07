f4fa6be2e64f42f13ba941e162b00ea1be5758ef
Han Byul <yahb9@naver.com>
2022-05-08 02:39:29 +0900

    Fix: Modal coomponent 에 컴포넌트 배치 및 todo 리스트 데이터 함수 props로 전달

--- src/components/AddTodo/Modal/Modal.jsx
+++ src/components/AddTodo/Modal/Modal.jsx
@@ -3,18 +3,59 @@ import styles from './Modal.module.scss'
 import CategoryModal from '../CategoryModal/CategoryModal'
 import classNames from 'classnames'
 import XBtn from '../MiddleBtn/XBtn'
+import Input from '../Input/Input'
+import DateBtn from '../MiddleBtn/DateBtn'
+import CategoryBtn from '../MiddleBtn/CategoryBtn'
+import Icons from '../Icons/Icons'
+import { useState } from 'react'
+import {dateFormat} from '../FunctionBundle'
 
 // eslint-disable-next-line react/prop-types
 function Modal({ onClose, onModalClick, active }) {
+  const [todoText, setTodoText] = useState('')
+  const [listData, setListData] = useState([])
+  const [formatDate, setFormatDate] = useState(dateFormat(new Date()))
+  const [selectCategory, setSelectCategory] = useState('All')
+  const [closeModal, setCloseModal] = useState(true)
+
+ const listAddData = () => {
+  const id = formatDate
+  const date = formatDate
+  const content = todoText
+  const category = selectCategory
+  const idDone = false
+ }
+
+  const handleInputChange = (e) => {
+    setTodoText(e.currentTarget.value)
+  }
+
+  const handleSelectDate = (date) => {
+    setFormatDate(dateFormat(date))
+   }
+
+   const handleModalClick = (bool) => {
+    setCloseModal(bool)
+   }
+
   return (
     <ModalPortal>
-      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
-      <div className={classNames(styles.background)} onClick={onModalClick}>
+      <div className={classNames(styles.background)} aria-hidden onClick={onModalClick}>
         <div className={classNames(styles.content)}>
           <div className={classNames(styles.page, { [styles.active]: active })}>
-            <div className={styles.xbtn}>
+            <div className={styles.xBtn}>
               <XBtn onClose={onClose} />
             </div>
+            <main className={styles.mainForm}>
+              <Input handleInputChange={handleInputChange} todoValue={todoText} />
+              <div className={styles.buttonForm}>
+                <DateBtn handleSelectDate={handleSelectDate} />
+                <CategoryBtn handleModalClick={handleModalClick} />
+              </div>
+              <div className={styles.modalBackground} style={closeModal ? {display: 'none'} : {display: 'inline-block'}} aria-hidden onClick={() => handleModalClick(true)} />
+              <div className={styles.categoryModal} aria-hidden style={closeModal ? {display: 'none'} : {display: 'inline-block'}}><CategoryModal handleModalClick={handleModalClick} handleSelectCategory={setSelectCategory} /></div>
+              <Icons />
+            </main>
           </div>
         </div>
       </div>
