03175e7df2516f625c91fb329bf3aa01d40c8dfc
Han Byul <yahb9@naver.com>
2022-05-08 02:53:56 +0900

    Fix: 컴포넌트  close 하는 handleModalClick와 카테고리 선택 전달 props 넘겨받기

--- src/components/AddTodo/CategoryModal/CategoryModal.js
+++ src/components/AddTodo/CategoryModal/CategoryModal.js
@@ -2,22 +2,20 @@ import { useState } from 'react'
 import styles from './Category.module.scss'
 import classnames from 'classnames'
 
-function CategoryModal({setSelectCategory, onModalClick}) {
+function CategoryModal({ handleModalClick, handleSelectCategory }) {
   const [todoCategory, setTodoCategory] = useState('')
   const [categoryList, setCategoryList] = useState(['All', 'Person', 'Business'])
-
-  //  const [selectCategory, setSelectCategory] = useState('All')
-   const [buttonToggle, setButtonToggle] = useState(0)
+  const [buttonToggle, setButtonToggle] = useState(0)
 
   const handleCategoryClick = (idx) => {
     setButtonToggle(idx)
-    // setSelectCategory(categoryList[idx])
+    handleSelectCategory(categoryList[idx])
   }
 
   const handleAddCategory = (e) => {
     e.preventDefault()
-    if (todoCategory !== ''){
-    setCategoryList((prevState) => [...prevState, todoCategory])
+    if (todoCategory !== '') {
+      setCategoryList((prevState) => [...prevState, todoCategory])
     }
     setTodoCategory('')
   }
@@ -29,21 +27,39 @@ function CategoryModal({setSelectCategory, onModalClick}) {
   return (
     <form className={styles.form} onSubmit={handleAddCategory}>
       <div className={styles.inputbox}>
-        <input className={styles.input} type='text' placeholder='create a Category' value={todoCategory} onChange={handleTextChange} />
-        <button className={styles.add} type='button' onClick={handleAddCategory}>+</button>
+        <input
+          className={styles.input}
+          type='text'
+          placeholder='create a Category'
+          value={todoCategory}
+          onChange={handleTextChange}
+        />
+        <button className={styles.add} type='button' onClick={handleAddCategory}>
+          +
+        </button>
       </div>
       <div className={styles.todolist}>
-        {categoryList.map((category, idx) => <button key={`task-${category}`} className={classnames(styles.category, {[styles.selectBtn] : buttonToggle === idx})} type='button' onClick={() => handleCategoryClick(idx)}>{category}</button>)}
+        {categoryList.map((category, idx) => (
+          <button
+            key={`task-${category}`}
+            className={classnames(styles.category, { [styles.selectBtn]: buttonToggle === idx })}
+            type='button'
+            onClick={() => handleCategoryClick(idx)}
+          >
+            {category}
+          </button>
+        ))}
       </div>
-      <button className={styles.select} type='button' onClick={onModalClick}>select</button>
+      <button className={styles.select} type='button' onClick={() => handleModalClick(true)}>
+        select
+      </button>
     </form>
   )
 }
 
-
 CategoryModal.propTypes = {
-  setSelectCategory: Function,
-  onModalClick: Function
+  handleModalClick: Function,
+  handleSelectCategory: Function
 }
 
 export default CategoryModal
