45f3134c91bb5e065eea05340c2c2e14b1bae19d
Han Byul <yahb9@naver.com>
2022-05-08 03:10:05 +0900

    Fix: className 수정

--- src/components/AddTodo/MiddleBtn/CategoryBtn.js
+++ src/components/AddTodo/MiddleBtn/CategoryBtn.js
@@ -1,19 +1,18 @@
 import MiddleBtn from "./MiddleBtn"
 import styles from './MiddleBtn.module.scss'
 
-function CategoryBtn ({onModalClick}) {
+function CategoryBtn ({handleModalClick}) {
   return (
     <MiddleBtn shape='circle'>
-      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
-      <div className={styles.background} onClick={onModalClick}>
-        <div className={styles.category} />
+      <div className={styles.outerCircle} aria-hidden onClick={() => handleModalClick(false)}>
+        <div className={styles.innerCircle} />
       </div>
     </MiddleBtn>
   )
 }
 
 CategoryBtn.propTypes = {
-  onModalClick: Function
+  handleModalClick: Function
 }
 
 export default CategoryBtn
